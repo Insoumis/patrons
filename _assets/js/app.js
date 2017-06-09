ed02f286c084fe6ae441e49e962b6e70edaa2e90
Buzugu <Buzugu@users.noreply.github.com>
2017-06-10 00:02:10 +0200

    Better menu sliding

--- _assets/js/app.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.menu-icon').addEventListener('click', function() {
-  var header = document.querySelector('header');
-  if (header.style.transform === "translateX(0%)") {
-    header.style.transform = "translateX(-101%)";
-  } else {
-    header.style.transform = "translateX(0%)";
-  }
-}, false);
